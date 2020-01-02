550d50562837fd355428c99d35c97061b4abe934
Theekshana Madushan <93.madushan@gmail.com>
2020-01-02 23:36:34 +0530

    add 4 print statments

--- Test.java
+++ Test.java
@@ -5,5 +5,9 @@ class Test
     public static void main(String args[]) 
     { 
         System.out.println("Hello"); 
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
+        System.out.println("Hello4"); 
     } 
 } 
