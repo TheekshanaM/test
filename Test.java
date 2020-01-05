263006e18130bdb5bb652d0ccd58954258751c93
Theekshana Madushan <93.madushan@gmail.com>
2020-01-05 11:54:17 +0530

    add 3 print statements

--- Test.java
+++ Test.java
@@ -4,8 +4,11 @@ class Test
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello"); 
+        System.out.println("Hello");
+        System.out.println("2.1");
+        System.out.println("2.2");
         System.out.println("Hello1");
-        System.out.println("Hello4"); 
+        System.out.println("Hello4");
+        System.out.println("2.3");
     } 
 } 
